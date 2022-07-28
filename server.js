30e835d167b17e7dfc923c301000fb3bfd64e804
AayushiA123 <61453846+AayushiA123@users.noreply.github.com>
2022-07-28 18:37:09 +0530

    small changes

---
+++ server.js
@@ -0,0 +1,18 @@
+var express=require('express');
+var bodyParser=require('body-parser');
+var router=require("./router");
+var app=express();
+
+
+//middleware configuration
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+<h2>This is simple project</h2>
+
+//associate router
+router(app);
+
+app.listen(7000);
+console.log("Producthub service listening on port 7000");
